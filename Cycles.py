d19aecde8205592f4d1232729ad99143db9ae9ed
Eugen Kobchuk <irravadi@gmail.com>
2023-03-26 11:26:54 +0300

    hm ex_3 done

--- Cycles.py
+++ Cycles.py
@@ -1,1 +1,13 @@
+a = int(input("Enter start of range: "))
 
+b = int(input("Enter end of range: "))
+
+for i in range(a, b + 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("Fizz Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
