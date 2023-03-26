c9f37e4ab6c8ffacae9b05cf2a9c07bafca3cc24
Eugen Kobchuk <irravadi@gmail.com>
2023-03-26 11:49:03 +0300

    hm ex_2 done

--- Cycles.py
+++ Cycles.py
@@ -1,9 +1,9 @@
-a = int(input('Enter num a: '))
-b = int(input('Enter num b: '))
+a = int(input('Enter a number a: '))
+b = int(input('Enter a number b: '))
 for i in range(a, b+1):
-    print(i)
+    print(i, end = '\t')
 for i in range(b, a-1, -1):
- print(i)
+ print(i, end = '\t')
 for i in range(a, b+1):
     if i%7 == 0:
         print(i)
@@ -11,4 +11,4 @@ counter = 0
 for i in range(a, b+1):
     if i%5 == 0:
         counter += 1
-        print(counter)
+print(counter)
