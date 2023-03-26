ee546c0ce2fe9474ec5226b341021d5dbb2a8bb6
Eugen Kobchuk <irravadi@gmail.com>
2023-03-26 11:42:30 +0300

    lab ex_5 done

--- Cycles.py
+++ Cycles.py
@@ -1,15 +1,15 @@
-a = int(input("Введення першого числа: "))
-b = int(input("Введення другого числа: "))
+a = int(input("Enter a number a: "))
+b = int(input("Enter a number b: "))
 if a>b:
  for i in range(b,a+1):          
    if i%2 == 1:
-     print(i)
+     print(i, end = "\t")
    else:
      pass
 
 if a<b:
  for i in range(a,b+1):          
    if i%2 == 1:
-    print(i)
+    print(i, end = "\t")
    else:
      pass
